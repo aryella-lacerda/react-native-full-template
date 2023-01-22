61dff1a745e2e7b8063cec1b767aa71c94e0d602
Aryella Lacerda <aryella.lacerda@gmail.com>
2023-01-22 17:07:43 -0300

    feat: install eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'standard-with-typescript',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native', 'react-hooks', 'jsx-a11y'],
+  rules: {
+    'react-native/no-raw-text': 'warn',
+  },
 };
