8bd330c2d4ec42f7fc9b6680f76cebe4bed1f627
Aryella Lacerda <aryella.lacerda@gmail.com>
2023-02-12 12:00:32 -0300

    fix: make babel recognize TS path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@services': './src/global/services',
+          '@navigation': './src/global/navigation',
+          '@features': './src/features',
+        },
+      },
+    ],
+  ],
 };
