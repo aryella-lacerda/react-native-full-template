5677af3c18c5744c6245501729914b3633118496
Aryella Lacerda <aryella.lacerda@gmail.com>
2023-02-25 19:00:05 -0300

    ci: add PAT instead of GH token

--- dangerfile.js
+++ dangerfile.js
@@ -2,11 +2,6 @@ const path = require('path');
 
 const {dangerReassure} = require('reassure');
 
-console.log('DEBUG');
-console.log(path);
-console.log(path.join);
-console.log(path.join(__dirname, '.reassure/output.md'));
-
 dangerReassure({
   inputFilePath: path.join(__dirname, '.reassure/output.md'),
 });
