f7c7cece28fe804ef0a4c2c6fbb11113f05a7b2f
Aryella Lacerda <aryella.lacerda@gmail.com>
2023-02-25 18:32:36 -0300

    ci: add debug to dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,6 +2,11 @@ import path from 'path';
 
 import {dangerReassure} from 'reassure';
 
+console.log('DEBUG');
+console.log(path);
+console.log(path.join);
+console.log(path.join(__dirname, '.reassure/output.md'));
+
 dangerReassure({
   inputFilePath: path.join(__dirname, '.reassure/output.md'),
 });
