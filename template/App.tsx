61e787efef1b742707630320f05865d3c3f1991e
Aryella Lacerda <aryella.lacerda@gmail.com>
2023-03-20 09:04:48 -0300

    feat: add react-native-codepush

--- template/App.tsx
+++ template/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import codePush from 'react-native-code-push';
 
 import {Root} from './Root';
 
@@ -15,4 +16,4 @@ function App(): JSX.Element {
   );
 }
 
-export default App;
+export default codePush(App);
