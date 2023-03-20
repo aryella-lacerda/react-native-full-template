d6a6b7531c6093266210a278ab53c04ba2ee2c7c
Aryella Lacerda <aryella.lacerda@gmail.com>
2023-03-20 09:19:33 -0300

    refactor: attempt to start a git repo on init

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
+const { exec } = require("child_process");
 
-console.log("This is post init script");
-git init
+console.log("Welcome to the Full Template!");
+
+exec("git init", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
